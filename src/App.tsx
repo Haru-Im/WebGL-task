fb1e916b2b6456ffd0f04cf9f8fae46654af6808
Haru-Im <chalk323@naver.com>
2023-11-03 15:14:42 +0900

    initialize canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
+import { MainPage } from './pages';
 
 export const App = () => {
-  return <div />;
+  return <MainPage />;
 };
---
+++ src/pages/type.ts
@@ -0,0 +1,17 @@
+import { EngineOptions, Scene, SceneOptions } from '@babylonjs/core';
+
+export type BabylonjsProps = {
+  antialias?: boolean;
+  engineOptions?: EngineOptions;
+  adaptToDeviceRatio?: boolean;
+  renderChildrenWhenReady?: boolean;
+  sceneOptions?: SceneOptions;
+  onSceneReady: (scene: Scene) => void;
+  /**
+   * Automatically trigger engine resize when the canvas resizes (default: true)
+   */
+  observeCanvasResize?: boolean;
+  onRender?: (scene: Scene) => void;
+  children?: React.ReactNode;
+  id?: string;
+};
