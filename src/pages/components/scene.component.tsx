d97ee02ea096a6c1e6043e8a915ad07716b2d94d
Haru-Im <chalk323@naver.com>
2023-11-03 17:05:09 +0900

    load glb file

--- src/pages/components/scene.component.tsx
+++ src/pages/components/scene.component.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useRef, FC } from 'react';
-import { Engine, Scene } from '@babylonjs/core';
+import {
+  ArcRotateCamera,
+  Engine,
+  HemisphericLight,
+  Scene,
+  SceneLoader,
+  Vector3,
+} from '@babylonjs/core';
 import { BabylonjsProps } from '../type';
+import '@babylonjs/loaders/glTF';
 
 type ISceneComponentProps = BabylonjsProps;
 
@@ -24,6 +32,32 @@ export const SceneComponent: FC<ISceneComponentProps> = ({
 
     const engine = new Engine(canvas, antialias, engineOptions, adaptToDeviceRatio);
     const scene = new Scene(engine, sceneOptions);
+
+    SceneLoader.ImportMeshAsync('', '/', 'sample.glb', scene);
+
+    // This creates and positions a free camera (non-mesh)
+    const camera = new ArcRotateCamera('Camera', 2, 1, 20, Vector3.Zero(), scene);
+
+    // This targets the camera to scene origin
+    // camera.setTarget(Vector3.Random());
+
+    // const canvas = scene.getEngine().getRenderingCanvas();
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'ground' shape.
+    // MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene);
+
+    // const helper = scene.createDefaultEnvironment();
+    // helper.setMainColor(Color3.Green());
+
     if (scene.isReady()) {
       onSceneReady(scene);
     } else {
