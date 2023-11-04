30f8fbb65835dddb7ec461b4541074b6ee159182
Haru-Im <chalk323@naver.com>
2023-11-04 20:34:55 +0900

    add feature moving right arm up and down

--- src/pages/components/scene.component.tsx
+++ src/pages/components/scene.component.tsx
@@ -9,6 +9,9 @@ import {
   SceneLoader,
   ShadowGenerator,
   Vector3,
+  Animation,
+  Quaternion,
+  Axis,
 } from '@babylonjs/core';
 import { BabylonjsProps } from '../type';
 import '@babylonjs/loaders/glTF';
@@ -54,10 +57,57 @@ export const SceneComponent: FC<ISceneComponentProps> = ({
     const model = SceneLoader.ImportMeshAsync('', '/', 'sample.glb', scene);
 
     model.then((res) => {
+      const rightArmNode = scene.getTransformNodeByName('CC_Base_R_Upperarm');
+
+      const startPosition = Quaternion.RotationAxis(Axis.Z, 0);
+      const handUp = Quaternion.RotationAxis(Axis.Z, Math.PI / 4);
+      const handUpAndDown = Quaternion.RotationAxis(Axis.Z, 0);
+      const handDown = Quaternion.RotationAxis(Axis.Z, -Math.PI / 4);
+      const handDownAndUp = Quaternion.RotationAxis(Axis.Z, 0);
+
+      const animFrameRate = 30;
+      const animTime = 1;
+      const nbFrames = animTime * animFrameRate - 1;
+
+      const animationRotation = new Animation(
+        'animationRotation',
+        'rotationQuaternion',
+        animFrameRate,
+        Animation.ANIMATIONTYPE_QUATERNION,
+        // Animation.ANIMATIONLOOPMODE_CYCLE,
+      );
+
+      const rotationKeys = [];
+
+      rotationKeys.push({
+        frame: 0,
+        value: startPosition,
+      });
+      rotationKeys.push({
+        frame: Math.floor(nbFrames / 4),
+        value: handUp,
+      });
+      rotationKeys.push({
+        frame: Math.floor(nbFrames / 2),
+        value: handUpAndDown,
+      });
+      rotationKeys.push({
+        frame: Math.floor((nbFrames * 3) / 4),
+        value: handDown,
+      });
+      rotationKeys.push({
+        frame: nbFrames,
+        value: handDownAndUp,
+      });
+
+      animationRotation.setKeys(rotationKeys);
+
+      rightArmNode.animations.push(animationRotation);
+      scene.beginAnimation(rightArmNode, 0, 30, false);
+
       res.meshes.forEach((mesh) => {
         mesh.position.y = -1;
         shadowGenerator.addShadowCaster(mesh);
-        console.log(123, mesh.material);
       });
     });
 
